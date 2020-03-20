0f1b954f8bc712ae4223625e1142c9f6d51ce7c3
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-03-20 08:12:37 -0500

    Vista general del player

--- src/routes/App.js
+++ src/routes/App.js
@@ -3,8 +3,9 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from '../containers/Home';
 import Login from '../containers/Login';
 import Register from '../containers/Register';
-import NotFound from  '../containers/NotFund';
-import Layout from '../components/Layout'
+import NotFound from '../containers/NotFund';
+import Player from '../containers/Player';
+import Layout from '../components/Layout';
 
 const App = () => (
   <BrowserRouter>
@@ -13,6 +14,7 @@ const App = () => (
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
+        <Route exact path="/player/:id" component={Player} />
         <Route component={NotFound} />
       </Switch>
     </Layout>
